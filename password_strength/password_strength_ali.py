5b19cb31ac682526795b915180d63e8ab9002c11
Iewnfod <shangzhiqiuyue@outlook.com>
2023-08-21 11:19:35 +0800

    Bug Fix: add auto generate of password

--- password_strength/password_strength_ali.py
+++ password_strength/password_strength_ali.py
@@ -4,43 +4,61 @@ q:
 1. 至少8位
 2. 至少一个大写字母，一个小写字母，一个数字，一个特殊字符
 3. 不能包含个人信息（个人信息可以事先通过 input 获取）
+同时，我们需要一个能够自动生成符合要求的密码的子程序，并让用户自己选择是否使用自动生成的密码
 '''
 
-import re
+import random
+import string
 
-def is_valid_password(password, personal_info):
-    # 至少8位
+def check_password(password):
     if len(password) < 8:
         return False
 
-    # 至少一个大写字母，一个小写字母，一个数字，一个特殊字符
-    has_upper = False
-    has_lower = False
-    has_digit = False
-    has_special = False
+    uppercase_letters = []
+    lowercase_letters = []
+    digits = []
+    special_characters = []
+
+    # 检查是否至少包含一个大写字母、一个小写字母、一个数字、一个特殊字符
     for char in password:
         if char.isupper():
-            has_upper = True
+            uppercase_letters.append(char)
         elif char.islower():
-            has_lower = True
+            lowercase_letters.append(char)
         elif char.isdigit():
-            has_digit = True
-        elif re.search("[@!#$%&'()*+,-./:;<=>?@[\]^_`{|}~]", char):
-            has_special = True
+            digits.append(char)
+        elif char in string.punctuation:
+            special_characters.append(char)
 
-    if not has_upper or not has_lower or not has_digit or not has_special:
+    # 检查密码中是否包含个人信息
+    if any(password.count(char) > 0 for char in ['123456', 'qwerty', 'password']) or any(char in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] for char in password):
         return False
 
-    # 不能包含个人信息
-    if any(char in personal_info for char in password):
-        return False
+    # 生成新的密码并检查是否符合要求
+    new_password = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(8))
 
-    return True
+    # 检查新密码是否符合要求
+    return check_password(new_password)
 
-# 判断一个密码是否符合条件
+def auto_generate_password(user_input):
+    if user_input == "true":
+        # 自动生成密码并返回
+        new_password = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(8))
+        return new_password
+    else:
+        # 提示用户输入自己的密码
+        return input("请输入自己的密码：")
+
+
 password = input("请输入密码：")
-personal_info = input("请输入个人信息：")
-if is_valid_password(password, personal_info):
-    print("密码符合条件")
+if check_password(password):
+    print("密码符合要求")
 else:
-    print("密码不符合条件")
+    print("密码不符合要求")
+
+user_input = input("是否使用自动生成的密码？(输入 true 或 false)：")
+new_password = auto_generate_password(user_input)
+if check_password(new_password):
+    print("自动生成的密码符合要求")
+else:
+    print("自动生成的密码不符合要求")
--- password_strength/password_strength_wzq.py
+++ password_strength/password_strength_wzq.py
@@ -1,22 +1,55 @@
 import re
+import string
+import random
 
-regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}'
+regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&`~\[\]\=\{\}])[A-Za-z\d$@$!%*?&`~\[\]\=\{\}]{8,}'
+special = list('$@$!%*?&`~[]=\{\}')
+
+def auto_generate(name: str):
+    all_chars = list(string.ascii_lowercase) + list(string.ascii_uppercase) + list(string.digits) + special
+    r = name
+    while name.lower() in r.lower() or check(r) == False:
+        r = ''.join(random.sample(all_chars, 12))
+    return r
+
+def check(password):
+    result = re.match(regex, password)
+    try:
+        if result.group() == password:
+            return True
+        else:
+            return False
+    except:
+        return False
 
 # 获取个人信息
 name = input('Name: ').strip()
-password = input('Password: ').strip()
+# 选择
+def choose():
+    k = int(input('\n1. Enter a Password\n2. Generate a Password\nChoose: '))
 
-# 判断无个人信息
-if name.lower() in password.lower():
-    print('No Personal Information in Password')
-    exit(0)
+    if k == 1:
+        password = input('Password: ').strip()
 
-# 正则匹配
-result = re.match(regex, password)
-try:
-    if result.group() == password:
-        print('Available Password')
+        # 判断无个人信息
+        if name.lower() in password.lower():
+            print('No Personal Information in Password')
+            exit(0)
+
+        # 正则匹配
+        if check(password):
+            print(f'Available Password\nHere is your password: \033[1m{password}\033[0m')
+        else:
+            print(f'Password Need at Lease one capital letter, one small case letter, a number and a special character. \nHere is a sample: \033[1m{auto_generate(name)}\033[0m')
+            choose()
+
+    elif k == 2:
+        password = auto_generate(name)
+        print(f'Here is your password: \033[1m{password}\033[0m')
+
     else:
-        print('Password Need at Lease one capital letter, one small case letter, a number and a special character')
-except:
-    print('Password Need at Lease one capital letter, one small case letter, a number and a special character')
+        print('Unknown Choose. Try again...')
+        choose()
+
+# main
+choose()
