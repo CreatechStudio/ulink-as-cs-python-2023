51e5342110e670c07ed39d44017bc09a4582acbb
lightumcc <jessiezhu@createchstudio.com>
2023-09-03 21:15:55 +0800

    add: tip_calculator_zzt

---
+++ tip_calculator/tip_calculator_zzt.py
@@ -0,0 +1,4 @@
+life_in_year = input("Please input your expected life in your year: ")
+lif_in_week = life_in_year*12*4
+life_in_day = life_in_year*365
+print(f"You have {life_in_year} years, {lif_in_week} weeks, and {life_in_day} days left.")
