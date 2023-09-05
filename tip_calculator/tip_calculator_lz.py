c73694935788b46de4dae4257b9939f718fda59f
soap <83483731+soap-tpst@users.noreply.github.com>
2023-09-05 13:13:45 +0800

    Revert "showing luca how to use git"
    This reverts commit 137d63274ac7714c8fc8350298ce17d264b7b77c.

--- tip_calculator/tip_calculator_lz.py
+++ tip_calculator/tip_calculator_lz.py
@@ -2,4 +2,3 @@ bill = float(input("What was the total bill?")[1:])
 per = int(input("What percentage tip would you like to give? 10, 12, or 15?"))
 peo = int(input("How many people to split the bill?"))
 print(f"Each person should pay: ${round(bill/100*per/peo + bill/peo, 2)}")
-# ashjkldghlaskdhgjlsa
